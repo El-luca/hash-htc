aa4fe65b7e9447cc40684f6f6365ce0c3eb31c5b
El-luca <lucas.lnss1994@gmail.com>
2022-05-09 22:49:27 -0300

    refactoring
    refactoring js and add draw

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,4 @@
-const $boardInten0 = document.querySelector('.board-iten-0')
-const $boardInten1 = document.querySelector('.board-iten-1')
-const $boardInten2 = document.querySelector('.board-iten-2')
-const $boardInten3 = document.querySelector('.board-iten-3')
-const $boardInten4 = document.querySelector('.board-iten-4')
-const $boardInten5 = document.querySelector('.board-iten-5')
-const $boardInten6 = document.querySelector('.board-iten-6')
-const $boardInten7 = document.querySelector('.board-iten-7')
-const $boardInten8 = document.querySelector('.board-iten-8')
+const $boardList = document.querySelectorAll('.board-list')
 const $reset = document.querySelector('.section__restart-button')
 
 let currentMove = 'X'
@@ -25,158 +17,101 @@ function toggleMove() {
 
 function verifyGame() {
   if (
-    $boardInten0.innerHTML != '' &&
-    $boardInten0.innerHTML == $boardInten1.innerHTML &&
-    $boardInten1.innerHTML == $boardInten2.innerHTML
+    $boardList[0].innerHTML != '' &&
+    $boardList[0].innerHTML == $boardList[1].innerHTML &&
+    $boardList[1].innerHTML == $boardList[2].innerHTML
   ) {
     return currentMove
   }
   if (
-    $boardInten3.innerHTML != '' &&
-    $boardInten3.innerHTML == $boardInten4.innerHTML &&
-    $boardInten4.innerHTML == $boardInten5.innerHTML
+    $boardList[3].innerHTML != '' &&
+    $boardList[3].innerHTML == $boardList[4].innerHTML &&
+    $boardList[4].innerHTML == $boardList[5].innerHTML
   ) {
     return currentMove
   }
   if (
-    $boardInten6.innerHTML != '' &&
-    $boardInten6.innerHTML == $boardInten7.innerHTML &&
-    $boardInten7.innerHTML == $boardInten8.innerHTML
+    $boardList[6].innerHTML != '' &&
+    $boardList[6].innerHTML == $boardList[7].innerHTML &&
+    $boardList[7].innerHTML == $boardList[8].innerHTML
   ) {
     return currentMove
   }
   if (
-    $boardInten0.innerHTML != '' &&
-    $boardInten0.innerHTML == $boardInten3.innerHTML &&
-    $boardInten3.innerHTML == $boardInten6.innerHTML
+    $boardList[0].innerHTML != '' &&
+    $boardList[0].innerHTML == $boardList[3].innerHTML &&
+    $boardList[3].innerHTML == $boardList[6].innerHTML
   ) {
     return currentMove
   }
   if (
-    $boardInten1.innerHTML != '' &&
-    $boardInten1.innerHTML == $boardInten4.innerHTML &&
-    $boardInten4.innerHTML == $boardInten7.innerHTML
+    $boardList[1].innerHTML != '' &&
+    $boardList[1].innerHTML == $boardList[4].innerHTML &&
+    $boardList[4].innerHTML == $boardList[7].innerHTML
   ) {
     return currentMove
   }
   if (
-    $boardInten2.innerHTML != '' &&
-    $boardInten2.innerHTML == $boardInten5.innerHTML &&
-    $boardInten5.innerHTML == $boardInten8.innerHTML
+    $boardList[2].innerHTML != '' &&
+    $boardList[2].innerHTML == $boardList[5].innerHTML &&
+    $boardList[5].innerHTML == $boardList[8].innerHTML
   ) {
     return currentMove
   }
   if (
-    $boardInten0.innerHTML != '' &&
-    $boardInten0.innerHTML == $boardInten4.innerHTML &&
-    $boardInten4.innerHTML == $boardInten8.innerHTML
+    $boardList[0].innerHTML != '' &&
+    $boardList[0].innerHTML == $boardList[4].innerHTML &&
+    $boardList[4].innerHTML == $boardList[8].innerHTML
   ) {
     return currentMove
   }
   if (
-    $boardInten2.innerHTML != '' &&
-    $boardInten2.innerHTML == $boardInten4.innerHTML &&
-    $boardInten4.innerHTML == $boardInten6.innerHTML
+    $boardList[2].innerHTML != '' &&
+    $boardList[2].innerHTML == $boardList[4].innerHTML &&
+    $boardList[4].innerHTML == $boardList[6].innerHTML
   ) {
     return currentMove
+  }
+
+  if(
+    $boardList[0].textContent != '' &&
+    $boardList[1].textContent != '' &&
+    $boardList[2].textContent != '' &&
+    $boardList[3].textContent != '' &&
+    $boardList[4].textContent != '' &&
+    $boardList[5].textContent != '' &&
+    $boardList[6].textContent != '' &&
+    $boardList[7].textContent != '' &&
+    $boardList[8].textContent != '' 
+  ){
+    return 'draw'
+  }
+
+}
+
+function move(boardIndex){
+  const $boardItem = $boardList[boardIndex]
+  if ($boardItem.innerHTML != '') {
+    return
+  }
+  $boardItem.innerHTML = currentMove
+  const gameresult = verifyGame()
+  if(gameresult == 'X' || gameresult == 'O'){
+      alert(currentMove)
+  }
+  if(gameresult == 'draw'){
+    alert('Empate')
+  }
+  toggleMove()
+}
+
+function addBoardListiners(){
+  for(let index = 0; index< $boardList.length; index++){
+    const $boardItem = $boardList[index ]
+    $boardItem.addEventListener('click', function(){
+      move(index)
+    })
   }
 }
-$boardInten0.addEventListener('click', function () {
-  if ($boardInten0.innerHTML != '') {
-    return
-  }
-  $boardInten0.innerHTML = currentMove
-  const gameresult = verifyGame()
-  if(gameresult == 'X' || gameresult == 'O'){
-      alert(currentMove)
-  }
-  toggleMove()
-})
-$boardInten1.addEventListener('click', function () {
-  if ($boardInten1.innerHTML != '') {
-    return
-  }
-  $boardInten1.innerHTML = currentMove
-  const gameresult = verifyGame()
-  if(gameresult == 'X' || gameresult == 'O'){
-      alert(currentMove)
-  }
-  toggleMove()
-})
-$boardInten2.addEventListener('click', function () {
-  if ($boardInten2.innerHTML != '') {
-    return
-  }
-  $boardInten2.innerHTML = currentMove
-  const gameresult = verifyGame()
-  if(gameresult == 'X' || gameresult == 'O'){
-      alert(currentMove)
-  }
-  toggleMove()
-})
-$boardInten3.addEventListener('click', function () {
-  if ($boardInten3.innerHTML != '') {
-    return
-  }
-  $boardInten3.innerHTML = currentMove
-  const gameresult = verifyGame()
-  if(gameresult == 'X' || gameresult == 'O'){
-      alert(currentMove)
-  }
-  toggleMove()
-})
-$boardInten4.addEventListener('click', function () {
-  if ($boardInten4.innerHTML != '') {
-    return
-  }
-  $boardInten4.innerHTML = currentMove
-  const gameresult = verifyGame()
-  if(gameresult == 'X' || gameresult == 'O'){
-      alert(currentMove)
-  }
-  toggleMove()
-})
-$boardInten5.addEventListener('click', function () {
-  if ($boardInten5.innerHTML != '') {
-    return
-  }
-  $boardInten5.innerHTML = currentMove
-  const gameresult = verifyGame()
-  if(gameresult == 'X' || gameresult == 'O'){
-      alert(currentMove)
-  }
-  toggleMove()
-})
-$boardInten6.addEventListener('click', function () {
-  if ($boardInten6.innerHTML != '') {
-    return
-  }
-  $boardInten6.innerHTML = currentMove
-  const gameresult = verifyGame()
-  if(gameresult == 'X' || gameresult == 'O'){
-      alert(currentMove)
-  }
-  toggleMove()
-})
-$boardInten7.addEventListener('click', function () {
-  if ($boardInten7.innerHTML != '') {
-    return
-  }
-  $boardInten7.innerHTML = currentMove
-  const gameresult = verifyGame()
-  if(gameresult == 'X' || gameresult == 'O'){
-      alert(currentMove)
-  }
-  toggleMove()
-})
-$boardInten8.addEventListener('click', function () {
-  if ($boardInten8.innerHTML != '') {
-    return
-  }
-  $boardInten8.innerHTML = currentMove
-  const gameresult = verifyGame()
-  if(gameresult == 'X' || gameresult == 'O'){
-      alert(currentMove)
-  }
-  toggleMove()
-})
+
+addBoardListiners()
