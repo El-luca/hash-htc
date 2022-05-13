de0fa4dfb81478a1884a047c0f279f353c0fd2e0
El-luca <lucas.lnss1994@gmail.com>
2022-05-13 20:25:30 -0300

    refatoring the verify winner
    add a refatoring in verifywinner  and add  print winner in screen

--- scripts/index.js
+++ scripts/index.js
@@ -1,108 +1,92 @@
 const $boardList = document.querySelectorAll('.board-list')
 const $reset = document.querySelector('.section__restart-button')
+const $scorePlayer1 = document.querySelector('.section__scoreboard--player1')
+const $scorePlayer2 = document.querySelector('.section__scoreboard--player2')
 
 let currentMove = 'X'
+let scorePlayer1 = 0
+let scorePlayer2 = 0
+
+const winConditions =[
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 $reset.addEventListener('click', function(){
     document.location.reload()
 })
 
 function toggleMove() {
-  if (currentMove == 'X') {
-    currentMove = 'O'
-  } else {
-    currentMove = 'X'
-  }
+ currentMove = currentMove == 'X' ? 'O' : 'X' // if ternário (usar só se a variavel for de forma condicional)
+  // if (currentMove == 'X') {
+  //   currentMove = 'O'
+  // } else {
+  //   currentMove = 'X'
+  // }
 }
 
 function verifyGame() {
-  if (
-    $boardList[0].innerHTML != '' &&
-    $boardList[0].innerHTML == $boardList[1].innerHTML &&
-    $boardList[1].innerHTML == $boardList[2].innerHTML
-  ) {
-    return currentMove
-  }
-  if (
-    $boardList[3].innerHTML != '' &&
-    $boardList[3].innerHTML == $boardList[4].innerHTML &&
-    $boardList[4].innerHTML == $boardList[5].innerHTML
-  ) {
-    return currentMove
-  }
-  if (
-    $boardList[6].innerHTML != '' &&
-    $boardList[6].innerHTML == $boardList[7].innerHTML &&
-    $boardList[7].innerHTML == $boardList[8].innerHTML
-  ) {
-    return currentMove
-  }
-  if (
-    $boardList[0].innerHTML != '' &&
-    $boardList[0].innerHTML == $boardList[3].innerHTML &&
-    $boardList[3].innerHTML == $boardList[6].innerHTML
-  ) {
-    return currentMove
-  }
-  if (
-    $boardList[1].innerHTML != '' &&
-    $boardList[1].innerHTML == $boardList[4].innerHTML &&
-    $boardList[4].innerHTML == $boardList[7].innerHTML
-  ) {
-    return currentMove
-  }
-  if (
-    $boardList[2].innerHTML != '' &&
-    $boardList[2].innerHTML == $boardList[5].innerHTML &&
-    $boardList[5].innerHTML == $boardList[8].innerHTML
-  ) {
-    return currentMove
-  }
-  if (
-    $boardList[0].innerHTML != '' &&
-    $boardList[0].innerHTML == $boardList[4].innerHTML &&
-    $boardList[4].innerHTML == $boardList[8].innerHTML
-  ) {
-    return currentMove
-  }
-  if (
-    $boardList[2].innerHTML != '' &&
-    $boardList[2].innerHTML == $boardList[4].innerHTML &&
-    $boardList[4].innerHTML == $boardList[6].innerHTML
-  ) {
-    return currentMove
-  }
+  let filledField = 0
+  for(const condition of winConditions){
+    const fieldIndex0 = condition[0]
+    const fieldIndex1 = condition[1]
+    const fieldIndex2 = condition[2]
 
-  if(
-    $boardList[0].textContent != '' &&
-    $boardList[1].textContent != '' &&
-    $boardList[2].textContent != '' &&
-    $boardList[3].textContent != '' &&
-    $boardList[4].textContent != '' &&
-    $boardList[5].textContent != '' &&
-    $boardList[6].textContent != '' &&
-    $boardList[7].textContent != '' &&
-    $boardList[8].textContent != '' 
+    const $field0 = $boardList[fieldIndex0]
+    const $field1 = $boardList[fieldIndex1]
+    const $field2 = $boardList[fieldIndex2]
+
+    if(
+    $field0.innerHTML != '' &&
+    $field0.innerHTML == $field1.innerHTML &&
+    $field1.innerHTML == $field2.innerHTML
   ){
-    return 'draw'
+    return currentMove
+    }
   }
 
+  for(const $field of $boardList){
+    if($field.innerHTML != '') filledField++
+  }
+  if (filledField == 9) return 'draw'
+}
+
+function resetBattlefield(){
+  for( const $boardItem of $boardList){
+    $boardItem.innerHTML = ''
+  }
 }
 
 function move(boardIndex){
   const $boardItem = $boardList[boardIndex]
-  if ($boardItem.innerHTML != '') {
-    return
-  }
+  if ($boardItem.innerHTML != '') return
   $boardItem.innerHTML = currentMove
   const gameresult = verifyGame()
   if(gameresult == 'X' || gameresult == 'O'){
-      alert(currentMove)
+    addpoint(gameresult)
+    printScore()
+    setTimeout(resetBattlefield, 1500)
   }
   if(gameresult == 'draw'){
-    alert('Empate')
+    setTimeout(resetBattlefield, 1500)
   }
   toggleMove()
+}
+
+function addpoint(winner){
+  if (winner == 'X') scorePlayer1++
+  if (winner == 'O') scorePlayer2++
+}
+
+function printScore(){
+  $scorePlayer1.innerHTML = String(scorePlayer1).padStart(2, '0') // por enquanto não usar ...
+  $scorePlayer2.innerHTML = scorePlayer2 < 10 ? '0' + scorePlayer2 : scorePlayer2
 }
 
 function addBoardListiners(){
